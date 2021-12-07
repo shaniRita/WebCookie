c09b27d88bda5cd4965451e96633b864149124dc
shaniRita <shanifreman@gmail.com>
2021-12-07 19:26:58 +0200

    Updated project

---
+++ README.md
@@ -0,0 +1,2 @@
+# web-project-g22
+Web project git repo for group 22
---
+++ main.js
@@ -0,0 +1,37 @@
+function change_location(loction) {
+    window.location.href = loction;
+}
+
+function find_location() {
+    console.log("sd");
+}
+
+function register_existUser() {
+console.log("some")
+    document.getElementById("existUser").display = block ;
+}
+
+function register_newUser() {
+    document.getElementById("newUser").display = block ;
+}
+
+
+function listen_to_questions(){
+
+    var faq = document.getElementsByClassName("faq-question");
+    var i;
+    for (i = 0; i < faq.length; i++) {
+        faq[i].addEventListener("click", function () {
+            /* Toggle between adding and removing the "active" class,
+            to highlight the button that controls the panel */
+            this.classList.toggle("active");
+            /* Toggle between hiding and showing the active panel */
+            var body = this.nextElementSibling;
+            if (body.style.display === "block") {
+                body.style.display = "none";
+            } else {
+                body.style.display = "block";
+            }
+        });
+    }
+}
